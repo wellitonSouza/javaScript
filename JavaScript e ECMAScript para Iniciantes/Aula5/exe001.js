6c5de9c05b8c3a13a6b7e8f62e8b01df1efc463d
Welliton <wellitonSouza>
2020-10-25 18:04:49 -0300

    Variáveis e tipos primitivos

---
+++ JavaScript e ECMAScript para Iniciantes/Aula5/exe001.js
@@ -0,0 +1,12 @@
+// Utilizar plugin Code Runner
+
+var name = 'Welliton';
+console.log(name);
+name = 'Paulo';
+console.log(name);
+
+// Typeof
+var n = 200;
+console.log(typeof n);
+n = 'Google';
+console.log(typeof n);
