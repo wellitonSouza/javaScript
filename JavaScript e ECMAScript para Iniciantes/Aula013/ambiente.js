5c21648ccac426040196c0213819689bbd1c2c0b
wellitonSouza <wellitonaz@yahoo.com.br>
2020-11-04 18:49:27 -0300

    Aula013 Repetição

---
+++ JavaScript e ECMAScript para Iniciantes/Aula013/ambiente.js
@@ -0,0 +1,16 @@
+/*
+console.log('Tudo bem!');
+console.log('Tudo bem!');
+*/
+var c = 0;
+
+while(c < 6){
+    console.log('Tudo bem?');
+    c++;
+}
+c = 1;
+do{
+    console.log(`Passo ${c}`);
+    c++;
+}while(c < 6)
+
